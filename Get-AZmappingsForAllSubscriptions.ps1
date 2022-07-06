2bae70b88bcbe46a1a5f92cc80433530d8aabe77
Jonsatchwell <josatchw@microsoft.com>
2022-07-07 09:42:43 +1000

    included switch for source to peer zone mapping

--- Get-AZmappingsForAllSubscriptions.ps1
+++ Get-AZmappingsForAllSubscriptions.ps1
@@ -5,10 +5,10 @@
 # ------                               ----                                 -------- - -------- - -------- -
 # yyyyyyyyy-e6e2-4539-93ae-xxxxxxxx    zzzzz-7fde-4caf-8629-aaaaaaaa         2         1         3
 
-Login-AzAccount
-$azContext = Get-AzContext
-# set Az context if required
-$sourceSubscription = $azContext.Subscription.id
+# Login-AzAccount
+# $azContext = Get-AzContext
+# # set Az context if required
+# $sourceSubscription = $azContext.Subscription.id
 
 # list all subscriptions or provide an array
 $subs = @('yyyyyyyyy-e6e2-4539-93ae-xxxxxxxx', 'zzzzz-7fde-4caf-8629-aaaaaaaa')
@@ -23,13 +23,24 @@ foreach ($sub in $subs) {
         continue 
     }
     $result = .\Check-AzureAZmapping.ps1 -Targetsubscription $sub -location $location
-    
+    $sourceAz1 = "" 
+    $sourceAz2 = "" 
+    $sourceAz3 = ""
+    # Switch ensures that the source AZ peer mapping returned is placed in the correct source variable
+    foreach ($item in $result) {
+        switch ($item.availabilityZone) {
+            1 { $sourceAz1 = $item.peers[0].availabilityZone }
+            2 { $sourceAz2 = $item.peers[0].availabilityZone }
+            3 { $sourceAz3 = $item.peers[0].availabilityZone }
+        }
+
+    }
     $Results += [PSCustomObject] @{
         Source    = $sourceSubscription
         Peer      = $result[0].peers[0].subscriptionId
-        SourceAz1 = $result[0].peers[0].availabilityZone
-        SourceAz2 = $result[1].peers[0].availabilityZone
-        SourceAz3 = $result[2].peers[0].availabilityZone
+        SourceAz1 = $sourceAz1
+        SourceAz2 = $sourceAz2
+        SourceAz3 = $sourceAz3
     }
 }
 # Print table
